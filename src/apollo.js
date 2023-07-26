67c23c3da40b68a4b58dda74ca47a4d682f3532f
alyona-shirpal <shirpalalona@gmail.com>
2023-07-26 16:28:34 +0300

    feat [set-up]  Set up the Apollo Client

---
+++ src/apollo.js
@@ -0,0 +1,9 @@
+import { ApolloClient, InMemoryCache } from "@apollo/client";
+import { GRAPHQL_API_ENDPOINT } from "./config";
+
+const client = new ApolloClient({
+  uri: GRAPHQL_API_ENDPOINT,
+  cache: new InMemoryCache()
+});
+
+export default client;
